6bdf06100128254557595fe6e9bbe9ad153d6404
MNikhil2004 <160840079+MNikhil2004@users.noreply.github.com>
2025-03-19 13:47:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:["https://67d5a61f91f46d36390f986d--careerinsight.netlify.app","http://localhost:3000"],
+  credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
